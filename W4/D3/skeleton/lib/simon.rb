4380160654882a3f2b6eb734d2e2dece796fee88
Dmitrii An <an.dmitriy93@gmail.com>
2020-12-08 23:32:44 -0800

    Fixed and game working perfectly

--- W4/D3/skeleton/lib/simon.rb
+++ W4/D3/skeleton/lib/simon.rb
@@ -1,4 +1,17 @@
+require_relative "user"
+module Screen
+  def self.clear_screen
+    print "\e[H\e[2J"
+    # print "\e[2J\e[F"
+  end
+
+  def self.clean_line
+    print "\r" + ("\e[A\e[K")
+  end
+end
+
 class Simon
+  extend Screen
   COLORS = %w(red blue green yellow)
 
   attr_accessor :sequence_length, :game_over, :seq
@@ -10,36 +23,58 @@ class Simon
   end
 
   def play
-    until @game_over == true
+    while @game_over == false
+      puts "Try Simon Game, remember as many as you can"
       take_turn
     end
     if @game_over == true
-        game_over_message 
-        reset_game
+      game_over_message
+      reset_game
     end
   end
 
   def take_turn
     if @game_over == false
+      Screen.clear_screen
       show_sequence
-      require_sequence
-      round_success_message
-      @sequence_length += 1
+      puts "Insert your guess and press Enter"
+      puts "Green = g, Red = r, Yellow = y, Blue = b"
+      if require_sequence == false
+        @game_over = true
+        return @game_over
+      else
+        round_success_message
+        @sequence_length += 1
+      end
     end
+    # Screen.clear
   end
 
   def show_sequence
     add_random_color
+    @seq.each do |color|
+      # print "\r" + ("\e[A\e[K")
+      Screen.clean_line
+      puts color
+      sleep(1)
+      # print "\r" + ("\e[A\e[K")
+      Screen.clean_line
+    end
   end
 
   def require_sequence
-
+    counter = 0
+    while counter < @seq.length
+      input = gets.chomp
+      return false if control_sequence[counter] != input
+      counter += 1
+      Screen.clean_line
+    end
+    true
   end
 
   def add_random_color
-    colors = %w(red blue yellow green)
-    @seq << colors.sample
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
@@ -48,6 +83,7 @@ class Simon
 
   def game_over_message
     puts "Game Over"
+    puts "Your correct answers were #{sequence_length-1} number"
   end
 
   def reset_game
@@ -55,4 +91,25 @@ class Simon
     @game_over = false
     @seq = []
   end
+
+  def control_sequence
+    control_arr = []
+    @seq.each do |i|
+      # color = "y" if @seq[i] == "yellow"
+      if i == "yellow"
+        control_arr << 'y'
+      elsif i == "red"
+        control_arr << 'r'
+      elsif i == "blue"
+        control_arr << 'b'
+      else
+        control_arr << 'g'
+      end
+    end
+    return control_arr
+  end
 end
+
+simon = Simon.new
+simon.play
+
